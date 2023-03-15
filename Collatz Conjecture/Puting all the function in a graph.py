92a1a92ccc585a24104497d6660227fe73794e4e
PeDro0210 <123851480+PeDro0210@users.noreply.github.com>
2023-03-14 22:46:48 -0600

    I changed everythin
    If it ain't working, it cause of that

--- Collatz Conjecture/Puting all the function in a graph.py
+++ Collatz Conjecture/Puting all the function in a graph.py
@@ -40,30 +40,35 @@ for i in range(1, 100):
 x=np.array(x, dtype=float)
 y=np.array(y, dtype=float)
 
-print(x.dtype)
-print(y.dtype)
+
 
 # Create Bezier curve paths between the points
+
+
 verts_list = []
 codes_list = []
-
 for i in range(0, len(x) - 1):
+    
     x1, y1 = x[i], y[i]
     x2, y2 = x[i+1], y[i+1]
-    x_mid = (x1 + x2) / 2
-    y_mid = (y1 + y2) / 2
+    x_mid = (x1 + x2) /2 #more undesrtandable curve is in 1.5
+    y_mid = (y1 + y2) /2 #more cool curve is in 2
     verts = np.array([(x1, y1), (x_mid, y1), (x_mid, y2), (x2, y2)], dtype=float)
     codes = [Path.MOVETO, Path.CURVE4, Path.CURVE4, Path.CURVE4]
     verts_list.append(verts)
     codes_list.append(codes)
+    print(i)
 
 # Plot Bezier curve patches
 fig, ax = plt.subplots()
 
+print("patching the graph")
 for verts, codes in zip(verts_list, codes_list):
     path = Path(verts, codes)
-    patch = PathPatch(path, facecolor='none', lw=2)
+    patch = PathPatch(path, facecolor='none', lw=1)
     ax.add_patch(patch)
+
+
 
 # Plot data points
 ax.plot(x, y, 'ro', markersize=5)
