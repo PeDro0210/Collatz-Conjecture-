57fb405ea7a6bb25dfd4662175d4d14fb4aae2df
PeDro0210 <123851480+PeDro0210@users.noreply.github.com>
2023-03-14 21:18:51 -0600

    Update Showing that all powers of 2 are valid.py

--- Collatz Conjecture/Showing that all powers of 2 are valid.py
+++ Collatz Conjecture/Showing that all powers of 2 are valid.py
@@ -1,4 +1,4 @@
-def collatz_variant(num, power):
+def comprobation(num, power):
     intermediate_nums = []
     while num != 1:
         if num in intermediate_nums:
@@ -6,6 +6,7 @@ def collatz_variant(num, power):
         intermediate_nums.append(num)
         if num % 2 == 0:
             num = num // 2
+            
         else:
             num = (num * 3 * power) + power
     return f"La secuencia termina en 1 con potencia {power}."
@@ -19,5 +20,5 @@ while True:
 
 for i in range(1, 100):
     power = 2**i
-    result = collatz_variant(num, power)
+    result = comprobation(num, power)
     print(result)
