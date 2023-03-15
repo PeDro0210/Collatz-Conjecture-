e8e44d69a85cd5ea6f7e46f5f46eeba26a44741a
PeDro0210 <123851480+PeDro0210@users.noreply.github.com>
2023-03-14 21:18:36 -0600

    Update Searching for multiples.py

--- Collatz Conjecture/Searching for multiples.py
+++ Collatz Conjecture/Searching for multiples.py
@@ -1,35 +1,44 @@
-def collatz(num, thenum, numerito):
+def multiples_checkear(num, thenum, numerito):
     count = 0
     previous_nums = []    
     while num != 1:
         if num % 2 == 0:
             num = num // 2
             print(num)
+            
         else:
             num = num * numerito + thenum
             print(num)
+            
         if num > 10**1000:
             thenum += 1
             numerito = 3 * thenum
             print(f"{thenum}*3, which is = {numerito}, is valid not for the conjuture")
-            return collatz(thenum, thenum, numerito)
+            return multiples_checkear(thenum, thenum, numerito)
+        #here is pretty explanatory, if the number is bigger that 10**1000, it means that is adding till infinity.
+        
         if num in previous_nums:
             count += 1
             if count >= 20:
                 thenum += 1
                 numerito = 3 * thenum
                 print(f"{thenum}*3, which is = {numerito}, is valid not for the conjuture")
-                return collatz(thenum, thenum, numerito)
+                return multiples_checkear(thenum, thenum, numerito)
+            
+            #in here we check if there's a loop in other part, if that's the case, we do 20 iterations and if it's still in a loop we show that the multiple
+            #is not part of the conjuture
+            
         else:
             previous_nums.append(num)
+            
     if num == 1:
         print(f"{thenum}*3, which is = {numerito}, is valid for the conjuture")
         thenum = 1 + thenum
         numerito = 3 * thenum
-        collatz(thenum, thenum, numerito)
+        multiples_checkear(thenum, thenum, numerito)
 
 
 user_num = int(input("Enter a number: "))
 thenum = 1
 numerito = 3 * thenum
-collatz(user_num, thenum, numerito)
+multiples_checkear(user_num, thenum, numerito)
